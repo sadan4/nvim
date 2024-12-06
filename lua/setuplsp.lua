5c5e7ede8ecf6cf014e61ef7a00a6abf91355d0f
sadan <117494111+sadan4@users.noreply.github.com>
2024-12-06 11:48:06 -0500

    add vimls

--- lua/setuplsp.lua
+++ lua/setuplsp.lua
@@ -20,6 +20,30 @@ local root_file = {
 	"eslint.config.mts",
 	"eslint.config.cts",
 }
+require("lspconfig").vimls.setup({
+	cmd = { "vim-language-server", "--stdio" },
+	filetypes = { "vim" },
+	init_options = {
+		diagnostic = {
+			enable = true,
+		},
+		indexes = {
+			count = 3,
+			gap = 100,
+			projectRootPatterns = { "runtime", "nvim", ".git", "autoload", "plugin" },
+			runtimepath = true,
+		},
+		isNeovim = true,
+		iskeyword = "@,48-57,_,192-255,-#",
+		runtimepath = "",
+		suggest = {
+			fromRuntimepath = true,
+			fromVimruntime = true,
+		},
+		vimruntime = "",
+	},
+	single_file_support = true,
+})
 require("lspconfig").rust_analyzer.setup({
 	settings = {
 		["rust-analyzer"] = {
