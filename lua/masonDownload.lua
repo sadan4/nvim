00a0f85ee8751143e09eeebfba088caa8773a79a
sadan <117494111+sadan4@users.noreply.github.com>
2025-03-26 22:20:53 +0000

    remove stupid mason thing

--- lua/masonDownload.lua
+++ lua/masonDownload.lua
@@ -1,15 +1,15 @@
-local pkgs = {
-	"jdtls",
-	"yaml-language-server",
-}
-local reg = require("mason-registry")
-for _, pname in ipairs(pkgs) do
-	local pkg = reg.get_package(pname)
-	if not pkg:is_installed() then
-		print(string.format("[masonDownload.lua]: %s is not installed. Installing.", pname))
-		local handle = pkg:install(nil)
-		--while handle.state ~= "CLOSED" do
-		--  print(handle.stdout)
-		-- end
-	end
-end
+-- local pkgs = {
+-- 	"jdtls",
+-- 	"yaml-language-server",
+-- }
+-- local reg = require("mason-registry")
+-- for _, pname in ipairs(pkgs) do
+-- 	local pkg = reg.get_package(pname)
+-- 	if not pkg:is_installed() then
+-- 		print(string.format("[masonDownload.lua]: %s is not installed. Installing.", pname))
+-- 		local handle = pkg:install(nil)
+-- 		--while handle.state ~= "CLOSED" do
+-- 		--  print(handle.stdout)
+-- 		-- end
+-- 	end
+-- end
